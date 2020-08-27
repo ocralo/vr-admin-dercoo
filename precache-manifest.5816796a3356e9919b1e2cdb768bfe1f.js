5e2fc33856496efc18e70d2eabf3930628b72b9e
Rodrigo Escobar <ocralo@gmail.com>
2020-08-27 17:52:31 -0500

    Add files via upload

---
+++ precache-manifest.5816796a3356e9919b1e2cdb768bfe1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd873c3d17226a769b9497b07127f366",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0cabe4aa62282ace6a00",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9b0d3bb3d2417ffa67f7",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0cabe4aa62282ace6a00",
+    "url": "./static/js/2.f250a43c.chunk.js"
+  },
+  {
+    "revision": "83cf22677282ef599fd4eb17fb28d692",
+    "url": "./static/js/2.f250a43c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b0d3bb3d2417ffa67f7",
+    "url": "./static/js/main.f879f86e.chunk.js"
+  },
+  {
+    "revision": "fc5fc6c3961f57201e3a",
+    "url": "./static/js/runtime-main.6dc1a488.js"
+  }
+]);
